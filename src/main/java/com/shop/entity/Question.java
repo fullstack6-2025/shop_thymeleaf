7bff37bc6bdf573b792fbabeb7e7c42b80a099ea
JangwooPark <p.jangwoo@gmail.com>
2025-06-12 15:31:47 +0900

    1

---
+++ src/main/java/com/shop/entity/Question.java
@@ -0,0 +1,29 @@
+package com.shop.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity		// DB의 테이블과 연결되어 있는 클래스 : 자바 클래스가 테이블과 연결 
+public class Question {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(length = 200)
+	private String subject;
+
+	@Column(columnDefinition = "TEXT")
+	private String content;
+
+	private LocalDateTime createDate;
+
+	private LocalDateTime modifyDate;
+
+	
+}
