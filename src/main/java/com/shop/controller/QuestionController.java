ac88adeca9c3bdb469261d78a0990ece0f872a52
JangwooPark <p.jangwoo@gmail.com>
2025-06-16 16:37:30 +0900

    1

--- src/main/java/com/shop/controller/QuestionController.java
+++ src/main/java/com/shop/controller/QuestionController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.shop.ShopThymeleafApplication;
 import com.shop.entity.Question;
 import com.shop.repository.QuestionRepository;
+import com.shop.service.QuestionService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,20 +31,26 @@ import lombok.RequiredArgsConstructor;
  */
 
 // Controller 의 역활 : 1. client의 요청을 받는다. => 2. 비즈니스 로직 처리 => 3. 뷰페이지 전송 
-
+@RequestMapping("/question")		// get, post 
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
+	// MVC 모델로 처리 
+	// client 요청 => Controller => Service => Repository => Entity => DB 
+		// service : Controller에서 Repository를 접근 할 경우 보안의 이슈 발생 
+					// Controller에서 직접 비즈니스 로직을 구현할 경우 중복코드가 많이 발생 될수 있다. 
+					// Service : 비즈니스 로직을 구현하면 중복된 코드와 보안을 강화 할 수 있다. 
+					// 유지 보수를 쉽게 할 수 있다. 
+//	private final QuestionRepository questionRepository;
 
-	private final QuestionRepository questionRepository;
-
+	private final QuestionService questionService; 
 	
-	@GetMapping("/question/list")				//http://localhost:8082/question/list    ( 1.client 요청 ) 
+	@GetMapping("/list")				//http://localhost:8082/question/list    ( 1.client 요청 ) 
 	public String list(Model model) {
 		
 		// 2. 비즈니스 로직 처리 (백엔드 로직 처리, ) 
 		List<Question> questionList = 
-				questionRepository.findAll(); 
+				questionService.getList(); 
 		
 		// 모델 객체에 변수의 값을 담아서 clinet 페이지로 전송 
 		model.addAttribute("questionList", questionList); 
