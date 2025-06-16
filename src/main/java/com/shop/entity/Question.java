eb5ab6541b1e3634ac85e84b17467407c07bd424
JangwooPark <p.jangwoo@gmail.com>
2025-06-16 11:05:11 +0900

    1

--- src/main/java/com/shop/entity/Question.java
+++ src/main/java/com/shop/entity/Question.java
@@ -1,12 +1,15 @@
 package com.shop.entity;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 @Getter
@@ -25,5 +28,8 @@ public class Question {
 	private String content;
 
 	private LocalDateTime createDate;
+	
+	@OneToMany(mappedBy="question" , cascade=CascadeType.REMOVE)
+	private List<Answer> answerList; 
 
 }
